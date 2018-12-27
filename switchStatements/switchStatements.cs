61c8c7e8feb877d171f24eba25135d978ec7a2d3
David Mevorah <mevorahde@hotmail.com>
2018-12-26 19:18:22 -0800

    Add exercises 16-17 from the FCC  C# Tutorial - Full Course for Beginners video

---
+++ switchStatements/switchStatements.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace switchStatements
+{
+    class switchStatements
+    {
+        static void Main(string[] args)
+        {
+            //print out the GetDay method with the passing parm of 0.
+            Console.WriteLine("The day you have chosen is: " + GetDay(0));
+            //print out the GetDay method with the passing parm of 4.
+            Console.WriteLine("The day you have chosen is: " + GetDay(4));
+            //Invalid number
+            Console.WriteLine("The day you have chosen is: " + GetDay(49));
+            Console.ReadLine();
+        }
+
+        //Create a method that will return a string value of the day of the week, passing one int parm.
+        static string GetDay(int dayNum)
+        {
+            //Create a variable for the name of the days of the weeks
+            string dayName;
+
+            //switch statements, 0-7 for the days of the week and set the dayName variable accordingly
+            switch (dayNum)
+            {
+                case 0:
+                    dayName = "Sunday";
+                    break;
+                case 1:
+                    dayName = "Monday";
+                    break;
+                case 2:
+                    dayName = "Tuesday";
+                    break;
+                case 3:
+                    dayName = "Wednesday";
+                    break;
+                case 4:
+                    dayName = "Thursday";
+                    break;
+                case 5:
+                    dayName = "Friday";
+                    break;
+                case 6:
+                    dayName = "Saturday";
+                    break;
+                default:
+                    dayName = "Invalid Day Number";
+                    break;
+            }
+            //return the dayName based on the case in the switch
+            return dayName;
+        }
+    }
+}
---
+++ whileLoops/whileLoops.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace whileLoops
+{
+    class whileLoops
+    {
+        static void Main(string[] args)
+        {
+            //example of a while loop
+            //create a variable and set it to 6
+            int index = 6;
+            //automatically run the code, then check the while condition
+            do
+            {
+                //print index's value and then add 1 to index
+                Console.WriteLine(index);
+                index++;
+            }
+            //When index is less than or equal to 5, loop through the code
+            while (index <= 5) ;
+
+            Console.ReadLine();
+        }
+    }
+}
